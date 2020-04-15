ebeb926cac5de64f0dce3431b272d5c600c08ff9
joshua-hurn <joshua.hurn@hotmail.com>
2020-04-15 14:02:21 -0500

    adding node but edges are not correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Graph from "react-graph-vis";
 
-const options = {
-  layout: {
-    hierarchical: false
-  },
-  edges: {
-    color: "#000000"
-  }
-};
+export default function App() {
+  const [nodes, setNodes] = useState([
+    { id: 1, label: "Node 1", title: "node 1 tootip text" },
+    { id: 2, label: "Node 2", title: "node 2 tootip text" },
+    { id: 3, label: "Node 3", title: "node 3 tootip text" },
+    { id: 4, label: "Node 4", title: "node 4 tootip text" },
+    { id: 5, label: "Node 5", title: "node 5 tootip text" }
+  ]);
+  const [edges, setEdges] = useState([
+    { from: 1, to: 2 },
+    { from: 1, to: 3 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 }
+  ]);
 
-const events = {
-  select: function (event) {
-    var { nodes, edges } = event;
-    console.log("Selected nodes:");
-    console.log(nodes);
-    console.log("Selected edges:");
-    console.log(edges);
-  }
-};
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+  const options = {
+    layout: {
+      hierarchical: true
+    },
+    edges: {
+      color: "#000000"
+    },
+    height: "500px"
+  };
 
-    this.state = {
-      graph: {
-        nodes: [
-          { id: 1, label: "Node 1", color: "#e04141" },
-          { id: 2, label: "Node 2", color: "#e09c41" },
-          { id: 3, label: "Node 3", color: "#e0df41" },
-          { id: 4, label: "Node 4", color: "#7be041" },
-          { id: 5, label: "Node 5", color: "#41e0c9" }
-        ],
-        edges: [{ from: 1, to: 2 }, { from: 1, to: 3 }, { from: 2, to: 4 }, { from: 2, to: 5 }]
-      }
+  const events = {
+    doubleClick: function (event) {
+      console.log(event);
+      setNodes([...nodes, { id: 6, label: "Node 6", color: "#fff" }])
     }
-  }
+  };
 
-  render() {
-    return (
-      <Graph graph={this.state.graph} options={options} events={events} style={{ height: "640px" }} />
-    );
-  }
+  return (
+    <Graph
+      graph={{ nodes, edges }}
+      options={options}
+      events={events}
+    // getNetwork={network => {
+    //   //  if you want access to vis.js network api you can set the state in a parent component using this property
+    // }}
+    />
+  );
 }
